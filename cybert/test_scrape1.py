f1396a64d870b0a9540a1bd20b0a93fb18d093bc
pedroandreou <pandreou01@gmail.com>
2022-06-09 14:11:10 +0300

    script for the first category of links

---
+++ cybert/test_scrape1.py
@@ -0,0 +1,22 @@
+from typer.testing import CliRunner
+import pytest
+
+from scrape1 import app
+
+runner = CliRunner()
+
+
+@pytest.fixture
+def output_path(tmp_path):
+    return tmp_path
+
+
+def test_app():
+    result = runner.invoke(
+        app,
+        [
+            "http://acerasanthropophorum.blogspot.com/2007/01/blog-post.html",
+            str(output_path),
+        ],
+    )
+    assert result.exit_code == 0
